44df68124c3d5c70fbc8cc423acbc5c056818446
Jack 郭 <guo@jackdemacbook-pro-2.local>
2018-06-07 11:57:58 +0800

    test

--- LIVE_test/ViewController.m
+++ LIVE_test/ViewController.m
@@ -18,10 +18,9 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
-    
-    
     self.view.backgroundColor = [UIColor blueColor];
     
+    NSLog(@"test");
 }
 
 
